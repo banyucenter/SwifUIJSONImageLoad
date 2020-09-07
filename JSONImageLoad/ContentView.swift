e45a7622bd4430779a889d879c8dea79595c4698
Ipung Purwono <ipungofficial@gmail.com>
2020-09-07 21:41:48 +0700

    Initial Commit

---
+++ JSONImageLoad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONImageLoad
+//
+//  Created by Ipung Dev Center on 07/09/20.
+//  Copyright © 2020 Banyu Center. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
